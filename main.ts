1fa4a067f22399bc4d8e761aa01f080a9304ab82
chrraffi <30281280+chrraffi@users.noreply.github.com>
2022-06-12 12:22:27 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    Anzahl_1 = 0
+    for (let Index = 0; Index <= Anz_würfeln - 1; Index++) {
+        Ergebnis = randint(1, 2)
+        if (Ergebnis == 1) {
+            Anzahl_1 += 1
+        }
+    }
+    basic.showNumber(Anzahl_1 / Anz_würfeln * 100)
+})
+let Ergebnis = 0
+let Anzahl_1 = 0
+let Anz_würfeln = 0
+Anz_würfeln = 10
+basic.forever(function () {
+	
+})
